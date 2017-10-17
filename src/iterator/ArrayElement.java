3061e0264cbe9ab6a42d1a8ba3b697ea1bf048c4
elena-maximenko <amely.honey@gmail.com>
2017-10-17 17:41:41 +0300

    ArrayElement.class

---
+++ src/iterator/ArrayElement.java
@@ -0,0 +1,16 @@
+package iterator;
+
+/**
+ * Created by User on 17.10.2017.
+ */
+public class ArrayElement {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
